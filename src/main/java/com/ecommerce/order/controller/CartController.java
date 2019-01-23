d68b21f545cfd4ea95a676c4159d7ffb43032b75
sahilkothari <sahilkothari114@gmail.com>
2019-01-23 11:07:24 +0530

    cart -> carts

--- src/main/java/com/ecommerce/order/controller/CartController.java
+++ src/main/java/com/ecommerce/order/controller/CartController.java
@@ -20,7 +20,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
 
-@RequestMapping("/cart")
+@RequestMapping("/carts")
 @RestController
 public class CartController {
     @Autowired
